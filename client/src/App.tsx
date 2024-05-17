39686ad3f1c9001b1291c02357c3b8f4ed31a96d
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-18 01:16:42 +0530

    Dark theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,22 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Compiler from "./pages/Compiler"
 import NotFound from "./pages/NotFound"
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   
 
   return (
     <>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/compiler" element={<Compiler/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/compiler" element={<Compiler/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </ThemeProvider>
+      
     
     </>
   )
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,19 @@
-
+import { Button } from "./ui/button"
+import { Link } from "react-router-dom"
 
 export default function Header() {
 return (
     <nav className="w-full h-[60px] bg-gray-900 text-white p-3 flex justify-between items-center">
-        <h2 className="font-bold">CodeStone</h2>
+        <Link to="/">
+            <h2 className="font-bold">CodeStone</h2>
+        </Link>
+        <ul className="flex gap-2">
+            <li>
+                <Link to="/compiler">
+                    <Button variant="secondary">Compiler</Button>
+                </Link>
+            </li>
+        </ul>
     </nav>
     )
 }
