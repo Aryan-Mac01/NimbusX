c30d052df5466fc071448d866933998c7b9bdb25
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-18 02:09:38 +0530

    javascript lang

--- client/src/components/CodeEditor.tsx
+++ client/src/components/CodeEditor.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { tags as t } from '@lezer/highlight';
+import { draculaInit } from '@uiw/codemirror-theme-dracula';
+import { loadLanguage, langNames, langs } from '@uiw/codemirror-extensions-langs';
 
 export default function CodeEditor() {
+    console.log('langNames:', langNames);
     const [value, setValue] = React.useState("console.log('hello world!');");
     const onChange = React.useCallback((val: any) => {
     console.log('val:', val);
     setValue(val);
     }, []);
     return (
-        <CodeMirror value={value} height="200px" extensions={[javascript({ jsx: true })]} onChange={onChange} />
+        <CodeMirror 
+        value={value} 
+        height="100vh" 
+        extensions={[loadLanguage('javascript')!]}  
+        onChange={onChange} 
+        theme={draculaInit({
+            settings: {
+            caret: '#c6c6c6',
+            fontFamily: 'monospace',
+            },
+            styles: [
+            { tag: t.comment, color: '#6272a4' },
+            ]
+        })}
+        />
     )
 }
