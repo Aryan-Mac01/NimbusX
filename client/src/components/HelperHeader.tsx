b3c83a25d56f4465686c9732b1e6edc128d27297
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-18 02:41:58 +0530

    Languages

---
+++ client/src/components/HelperHeader.tsx
@@ -0,0 +1,44 @@
+import { Save, Share2 } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function HelperHeader() {
+  return (
+    <div className="__helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center">
+      <div className="__btn_container flex gap-2">
+        <Button
+          className="flex justify-center items-center gap-1"
+          variant="success"
+        >
+          <Save />
+          Save
+        </Button>
+        <Button
+          className="flex justify-center items-center gap-1"
+          variant="secondary"
+        >
+          <Share2 size={16} />
+          Share
+        </Button>
+      </div>
+      <div className="__tab_switcher">
+        <Select defaultValue="html">
+          <SelectTrigger className="w-[180px] bg-gray-800 outline-none focus:ring-0">
+            <SelectValue/>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="html">HTML</SelectItem>
+            <SelectItem value="css">CSS</SelectItem>
+            <SelectItem value="javascript">JavaScript</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Compiler.tsx
+++ client/src/pages/Compiler.tsx
@@ -1,10 +1,12 @@
 import CodeEditor from "@/components/CodeEditor"
+import HelperHeader from "@/components/HelperHeader"
 import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable"
 
     export default function Compiler() {
         return (
             <ResizablePanelGroup direction="horizontal">
         <ResizablePanel className="h-[calc(100dvh-60px)] min-w-[350px]" defaultSize={50}>
+            <HelperHeader/>
             <CodeEditor/>
         </ResizablePanel>
         <ResizableHandle />
