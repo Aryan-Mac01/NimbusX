51641f0a7b204bd710ba09497d93fd06c1734c34
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-21 22:20:14 +0530

    Update language

--- client/src/components/CodeEditor.tsx
+++ client/src/components/CodeEditor.tsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import CodeMirror from '@uiw/react-codemirror';
-import { tags as t } from '@lezer/highlight';
-import { draculaInit } from '@uiw/codemirror-theme-dracula';
-import { loadLanguage } from '@uiw/codemirror-extensions-langs';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { tags as t } from "@lezer/highlight";
+import { draculaInit } from "@uiw/codemirror-theme-dracula";
+import { loadLanguage } from "@uiw/codemirror-extensions-langs";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 export default function CodeEditor() {
-    const currentLanguage = useSelector(
-        (state: RootState) => state.compilerSlice.currentLanguage
-      );currentLanguage
-    const [value, setValue] = React.useState("console.log('hello world!');");
-    const onChange = React.useCallback((val: any) => {
-    console.log('val:', val);
-    setValue(val);
-    }, []);
-    return (
-        <CodeMirror 
-        value={value} 
-        height="100vh" 
-        extensions={[loadLanguage(currentLanguage)!]}  
-        onChange={onChange} 
-        theme={draculaInit({
-            settings: {
-            caret: '#c6c6c6',
-            fontFamily: 'monospace',
-            },
-            styles: [
-            { tag: t.comment, color: '#6272a4' },
-            ]
-        })}
-        />
-    )
+  const currentLanguage = useSelector(
+    (state: RootState) => state.compilerSlice.currentLanguage
+  );
+
+  const currentCode = useSelector(
+    (state: RootState) => state.compilerSlice.currentCode
+  );
+
+  const dispatch = useDispatch();
+  const [value, setValue] = React.useState("console.log('hello world!');");
+  const onChange = React.useCallback((value: string) => {
+    // console.log('val:', val);
+    // setValue(val);
+  }, []);
+  return (
+    <CodeMirror
+      value={currentCode}
+      height="calc(100vh - 60px - 50px)"
+      extensions={[loadLanguage(currentLanguage)!]}
+      onChange={onChange}
+      theme={draculaInit({
+        settings: {
+          caret: "#c6c6c6",
+          fontFamily: "monospace",
+        },
+        styles: [{ tag: t.comment, color: "#6272a4" }],
+      })}
+    />
+  );
 }
--- client/src/redux/slices/compilerSlice.ts
+++ client/src/redux/slices/compilerSlice.ts
@@ -1,29 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { PayloadAction } from "@reduxjs/toolkit/react";
+import { act } from "react";
 
 export interface CompilerSliceStateType {
-  html: string;
-  css: string;
-  javascript: string;
+  fullCode: {
+    html: string;
+    css: string;
+    javascript: string;
+  };
+
   currentLanguage: "html" | "css" | "javascript ";
+  currentCode: string;
 }
 
-const initialState:CompilerSliceStateType  = {
-  html: "",
-  css: "",
-  javascript: "",
+const initialState: CompilerSliceStateType = {
+  fullCode: {
+    html: "This is HTML Code",
+    css: "This is CSS Code",
+    javascript: "This is JS Code",
+  },
   currentLanguage: "html",
+  currentCode: "",
 };
 
 const compilerSlice = createSlice({
   name: "compilerSlice",
   initialState,
   reducers: {
-    updateCurrentLanguage: (state, action: PayloadAction<CompilerSliceStateType["currentLanguage"]>) => {
+    updateCurrentLanguage: (
+      state,
+      action: PayloadAction<CompilerSliceStateType["currentLanguage"]>
+    ) => {
       state.currentLanguage = action.payload;
+    },
+    updateCodeValue: (
+      state,
+      action: PayloadAction<{
+        language: CompilerSliceStateType["currentLanguage"];
+        code: string;
+      }>
+    ) => {
+      const { code, language } = action.payload;
+      state.fullCode[language] = code;
+    },
+    updateCurrentCode: (state, action: PayloadAction<string>) => {
+      state.currentCode = action.payload;
     },
   },
 });
 
 export default compilerSlice.reducer;
-export const { updateCurrentLanguage } = compilerSlice.actions;
+export const { updateCurrentLanguage, updateCodeValue, updateCurrentCode } =
+  compilerSlice.actions;
