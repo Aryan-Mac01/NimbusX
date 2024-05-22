6fdc127bb7f14f8d5add545a150a506136db491c
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-22 10:22:08 +0530

    Adding iframe to the compiler

--- client/src/components/RenderCode.tsx
+++ client/src/components/RenderCode.tsx
@@ -2,9 +2,29 @@ import { RootState } from "@/redux/store";
 import { useSelector } from "react-redux";
 
 export default function RenderCode() {
-    const fullcode = useSelector((state: RootState)=>state.compilerSlice.fullCode)
-  return 
+  const fullcode = useSelector(
+    (state: RootState) => state.compilerSlice.fullCode
+  );
+  const combinedCode = `
+  <html>
+    <style>
+    ${fullcode.css}
+    </style>
+    <body>
+    ${fullcode.html}
+    </body>
+    <script>
+    ${fullcode.javascript}
+    </script>
+  </html>
+  `;
+
+  const iframeCode = `data:text/html;charset=utf-8,${encodeURIComponent(
+    combinedCode
+  )}`
+  return(
   <div>
-    <iframe/>
-  </div>;
+    <iframe src={iframeCode}/>
+  </div>
+  );
 }
--- client/src/pages/Compiler.tsx
+++ client/src/pages/Compiler.tsx
@@ -1,16 +1,29 @@
-import CodeEditor from "@/components/CodeEditor"
-import HelperHeader from "@/components/HelperHeader"
-import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable"
+import CodeEditor from "@/components/CodeEditor";
+import HelperHeader from "@/components/HelperHeader";
+import RenderCode from "@/components/RenderCode";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "@/components/ui/resizable";
 
-    export default function Compiler() {
-        return (
-            <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel className="h-[calc(100dvh-60px)] min-w-[350px]" defaultSize={50}>
-            <HelperHeader/>
-            <CodeEditor/>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel className="h-[calc(100dvh-60px)] min-w-[350px]" defaultSize={50}>Right Side</ResizablePanel>
-        </ResizablePanelGroup>
-        )
-    }
+export default function Compiler() {
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel
+        className="h-[calc(100dvh-60px)] min-w-[350px]"
+        defaultSize={50}
+      >
+        <HelperHeader />
+        <CodeEditor />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel
+        className="h-[calc(100dvh-60px)] min-w-[350px]"
+        defaultSize={50}
+      >
+        <RenderCode/>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
