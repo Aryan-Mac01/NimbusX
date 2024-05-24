c7f248ea00eb2ee08947a4b384d46bbfc6927390
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-24 09:49:44 +0530

    Save button working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import { dbConnect } from "./lib/dbConnect";
@@ -10,9 +10,14 @@ app.use(express.json());
 app.use(cors());
 config();
 
+app.get("/",(req:Request, res:Response)=>{
+  return res.status(200).send("OK ")
+  
+})
+
 app.use("/compiler", compilerRouter)
 
 dbConnect();
 app.listen(4000, () => {
-  console.log("http://localhost:4000");
+  console.log("http://0.0.0.0:4000");
 });
